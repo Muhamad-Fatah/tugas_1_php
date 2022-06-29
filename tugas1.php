ffa0fcde269a44de2728a2d31ec7edb401fed1d3
Muhamad Fatah <muhamad.fatah494@gmail.com>
2022-06-29 11:43:22 +0700

    tugas 1 php
    tugas 1 php - niomic

---
+++ tugas1.php
@@ -0,0 +1,25 @@
+<?php
+$ayamG = "Rp. 13000";
+$ayamB = "Rp. 15000";
+$ayamS = "Rp. 13000";
+$tempeG = "Rp. 1000";
+$tahuG = "Rp. 1000";
+$soto = "Rp. 3000";
+$nasiP = "Rp. 5000";
+$esTeh = "Rp. 4000";
+$esTebu = "Rp. 5000";
+?>
+<div>
+    <h2>DAFTAR MENU</h2>
+    <ul>
+        <li><?php echo "Ayam Goreng = $ayamG" ?></li>
+        <li><?php echo "Ayam Bakar = $ayamB" ?></li>
+        <li><?php echo "Ayam Sayur = $ayamS" ?></li>
+        <li><?php echo "Tempe Goreng = $tempeG" ?></li>
+        <li><?php echo "Tahu Goreng = $tahuG" ?></li>
+        <li><?php echo "Soto = $soto" ?></li>
+        <li><?php echo "Nasi Putih = $nasiP" ?></li>
+        <li><?php echo "Es Teh = $esTeh" ?></li>
+        <li><?php echo "Es Tebu = $esTebu" ?></li>
+    </ul>
+</div>
---
+++ tugas1_2.php
@@ -0,0 +1,40 @@
+<?php
+   $namaMenu = array("Cireng","Es Duren","Es Milo", "Es Teh", "Piscok");
+   $hargaMenu = array("Rp. 1000","Rp. 15000", "Rp. 20000", "Rp. 5000", "Rp. 1000");
+?>
+
+<h2>DAFTAR MENU </h2>
+<table border="1">
+<thead>
+    <th><?php echo"No"?></th>
+    <th><?php echo"Nama Menu"?></th>
+    <th><?php echo"Harga"?></th>
+</thead>
+<tbody>
+<tr>
+    <td><?php echo"1"?></td>
+    <td><?php echo"$namaMenu[0]"?></td>
+    <td><?php echo"$hargaMenu[0]"?></td>
+</tr>
+<tr>
+<td><?php echo"2"?></td>
+<td><?php echo"$namaMenu[1]"?></td>
+<td><?php echo"$hargaMenu[1]"?></td>
+</tr>
+<tr>
+    <td><?php echo"3"?></td>
+    <td><?php echo"$namaMenu[2]"?></td>
+    <td><?php echo"$hargaMenu[2]"?></td>
+</tr>
+<tr>
+    <td><?php echo"4"?></td>
+    <td><?php echo"$namaMenu[3]"?></td>
+    <td><?php echo"$hargaMenu[3]"?></td>
+</tr>
+<tr>
+    <td><?php echo"5"?></td>
+    <td><?php echo"$namaMenu[4]"?></td>
+    <td><?php echo"$hargaMenu[4]"?></td>
+</tr>
+</tbody>
+</table>
